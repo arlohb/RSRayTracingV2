2949228ce3bc40cac38ddec513128940f73cd2dd
arlohb <arlo@urwick.co.uk>
2022-03-19 11:49:42 +0000

    Changed layout slightly

--- src/app.rs
+++ src/app.rs
@@ -38,26 +38,10 @@ impl epi::App for TemplateApp {
 
   /// Called each time the UI needs repainting, which may be many times per second.
   /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-  fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
+  fn update(&mut self, ctx: &egui::Context, _frame: &epi::Frame) {
     let Self { label, value } = self;
 
-    // Examples of how to create different panels and windows.
-    // Pick whichever suits you.
-    // Tip: a good default choice is to just keep the `CentralPanel`.
-    // For inspiration and more examples, go to https://emilk.github.io/egui
-
-    egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-      // The top panel is often a good place for a menu bar:
-      egui::menu::bar(ui, |ui| {
-        ui.menu_button("File", |ui| {
-          if ui.button("Quit").clicked() {
-            frame.quit();
-          }
-        });
-      });
-    });
-
-    egui::SidePanel::left("side_panel").show(ctx, |ui| {
+    egui::SidePanel::right("side_panel").show(ctx, |ui| {
       ui.heading("Side Panel");
 
       ui.horizontal(|ui| {
