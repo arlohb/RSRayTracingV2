ed9941b5e41795d04609fc904bffc4f05364f72b
arlohb <arlo@urwick.co.uk>
2022-03-19 17:07:15 +0000

    Threading! Now 3-4 times faster

--- src/ray_tracer.rs
+++ src/ray_tracer.rs
@@ -1,3 +1,5 @@
+use rayon::prelude::*;
+
 use crate::{
   vec3::Vec3,
   objects::{
@@ -145,18 +147,17 @@ impl RayTracer {
     let height_world_space = (image_plane.top - image_plane.bottom).length();
     let (right, up, _) = camera.get_vectors();
 
-    for x in 0..self.width {
-      for y in 0..self.height {
-        let pixel = self.render_pixel(x, y, top_left_point, width_world_space, height_world_space, right, up, &camera);
+    image.pixels.par_iter_mut().enumerate().for_each(|(index, colour)| {
+      let y = (index as u32) / (self.width as u32);
+      let x = index as u32 % self.width;
 
-        let index = (x + (y * self.width)) as usize;
+      let pixel = self.render_pixel(x, y, top_left_point, width_world_space, height_world_space, right, up, &camera);
 
-        image.pixels[index] = eframe::epaint::Color32::from_rgb(
-          (pixel.0 * 255.) as u8,
-          (pixel.1 * 255.) as u8,
-          (pixel.2 * 255.) as u8,
-        );
-      }
-    }
+      *colour = eframe::epaint::Color32::from_rgb(
+        (pixel.0 * 255.) as u8,
+        (pixel.1 * 255.) as u8,
+        (pixel.2 * 255.) as u8,
+      );
+    });
   }
 }
