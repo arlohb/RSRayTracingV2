b1826f012320cc9742f4d1fbb1075752a8a0dc6a
arlohb <arlo@urwick.co.uk>
2022-03-22 17:00:36 +0000

    Added some notes to start_server.sh about web multithreading

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 set -eu
 
+# I need custom headers to enable multithreading in the web
+
+# cargo basic-http-server does not allow for custom headers
+
+# If I instead used node http-server,
+# This PR would allow for custom headers
+# https://github.com/http-party/http-server/pull/282
+# But it been LITERALLY 6 YEARS without being merged.
+# I could just use the PR,
+# but a better solution would be great
+# http-server has SO MANY forks, so maybe one of them has this
+
+# Perhaps I need to make my own simple server,
+# but there's got to be one out there that allows for custom headers
+
+# This python script can do it with Simple HTTP Server
+# https://gist.github.com/daimatz/3ca32ae11d9635372853
+# this is probably best
+
 cargo install basic-http-server
 
 # https://stackoverflow.com/questions/21336126/linux-bash-script-to-extract-ip-address
