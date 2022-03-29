a40106abbfaa8b0ced07e0f6192248be29e9384b
arlohb <arlo@urwick.co.uk>
2022-03-29 23:01:41 +0100

    HTTPS?

--- web/server.js
+++ web/server.js
@@ -1,17 +1,19 @@
-import { createServer } from "http";
-import { readFile } from "fs";
+import { createServer as createServerHTTP } from "http";
+import { createServer as createServerHTTPS } from "https";
+import { readFile, readFileSync } from "fs";
 import { lookup } from "mime-types";
 
 const port = 8080;
 const webFolder = "dist";
 
-createServer((request, response) => {
+const app = (request, response) => {
   const url = request.url === "/" ? "/index.html" : request.url;
   const filename = `${process.cwd()}/${webFolder}${url}`;
   // lookup can deal with files in folders etc
   const mimeType = lookup(filename);
   console.log(`Request for ${filename} with mime type ${mimeType}`);
 
+  // should promisify this
   readFile(filename, null, (err, data) => {
     if (err) {
       response.writeHead(404, {"Content-Type": "text/plain"});
@@ -30,6 +32,21 @@ createServer((request, response) => {
     response.write(data);
     response.end();
   })
-}).listen(port);
+};
+
+createServerHTTP(app).listen(port);
+
+try {
+  // should promisify this
+  const { key, cert } = JSON.parse(readFileSync("secrets.json"));
+
+  createServerHTTPS({
+    // should promisify this
+    key: fs.readFileSync(key),
+    cert: fs.readFileSync(cert),
+  }, app).listen(port + 1);
+} catch {
+  console.log("HTTPS failed");
+}
 
 console.log("Static file server running at http://localhost:" + port + "/\nCTRL + C to shutdown");
